508e766596ff619c0d4b10759e01edb0aa8850ce
Arun Kumar Singh <arunkrasingh@gmail.com>
2024-03-01 09:24:22 +0530

    add refresh token end point

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,13 +1,20 @@
-const asyncHandler = (requestHandler)=>{
-    return (req,res,next)=>{
-        Promise.resolve(requestHandler(req,res,next)).catch((error)=>next(error))
+// const asyncHandler = (requestHandler)=>{
+//     return (req,res,next)=>{
+//         Promise.resolve(requestHandler(req,res,next)).catch((error)=>next(error))
+//     }
+// }
+
+// export {asyncHandler}
+
+
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
     }
 }
 
-export {asyncHandler}
 
-
-
+export { asyncHandler }
 
 
 
